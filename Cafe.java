0b4c4d0e44704d2ec8ac30da0d5d5aeac9eb61fc
rlong711 <you@example.com>
2023-03-28 22:38:06 -0400

    main coding done

--- Cafe.java
+++ Cafe.java
@@ -1,8 +1,31 @@
 /* This is a stub for the Cafe class */
 public class Cafe extends Building {
 
-    public Cafe() {
+    private int nCoffeeOunces; 
+    private int nSugarPackets; 
+    private int nCreams;
+    private int nCups; 
+
+    public Cafe(String name, String address, int nFloors) {
+        super(name,address,nFloors); 
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets; 
+        this.nCreams = nCreams; 
+        this.nCups = nCups;  
+
         System.out.println("You have built a cafe: ☕");
+    }
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams){
+        this.nCoffeeOunces -= size; 
+        this.nSugarPackets -= nSugarPackets; 
+        this.nCreams -= nCreams; 
+    }
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCups){
+        this.nCoffeeOunces += nCoffeeOunces; 
+        this.nSugarPackets += nSugarPackets; 
+        this.nCups += nCups; 
     }
     
     public static void main(String[] args) {
--- Library.java
+++ Library.java
@@ -12,6 +12,37 @@ public class Library extends Building {
       System.out.println("You have built a library: 📖");
     }
 
+    public void addTitle(String title) {
+      this.collection.put(title, true); 
+    }
+
+    public String removeTitle(String title) {
+       this.collection.remove(title); 
+       return(this.collection.toString()); 
+
+    }
+
+    public void checkOut(String title){
+      this.collection.replace(title, true, false); 
+    }
+
+    public void returnBook(String title){
+      this.collection.replace(title, false, true); 
+    }
+
+    public boolean containsTitle(String title){
+      return(this.collection.contains(title)); 
+    }
+
+    public boolean isAvailable(String title) {
+      return(this.collection.get(title)); 
+    }
+
+    public void printCollection(){
+      System.out.println(this.collection); 
+    }
+
+
   
     public static void main(String[] args) {
       new Library();
